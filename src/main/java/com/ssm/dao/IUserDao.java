04ba90d51302c6b5dbac549955266889dcced2d2
liniawu <280608701@qq.com>
2019-09-28 18:57:37 +0800

    ssm first

---
+++ src/main/java/com/ssm/dao/IUserDao.java
@@ -0,0 +1,6 @@
+package com.ssm.dao;
+import com.ssm.model.User;
+public interface IUserDao {
+    User selectUser(long id);
+}
+
---
+++ src/main/java/com/ssm/service/IUserService.java
@@ -0,0 +1,13 @@
+package com.ssm.service;
+
+import com.ssm.model.User;
+
+
+public interface IUserService {
+
+    User selectUser(long userId);
+
+    int insert(User user);
+
+}
+
